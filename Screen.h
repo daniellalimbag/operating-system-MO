51b151b42bcd4eb1812b536b4fec5a92c9c8cebd
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2025-05-15 17:08:52 +0800

    feat: added timestamp logic

--- Screen.h
+++ Screen.h
@@ -1,20 +1,33 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
 
 class Screen {
     std::string processName;
     std::string timestamp;
     int currentLine;
     int totalLines;
+
 public:
-    Screen(const std::string& name, const std::string& time, int total = 5) //placeholder 5 lines
-        : processName(name), timestamp(time), currentLine(1), totalLines(total) {}
+    Screen() = delete;
+    Screen(const std::string& name, int total = 5)
+        : processName(name), currentLine(1), totalLines(total) {
+        auto now = std::chrono::system_clock::now();
+        auto now_time_t = std::chrono::system_clock::to_time_t(now);
+        std::tm local_tm;
+        localtime_s(&local_tm, &now_time_t);
+        std::ostringstream oss;
+        oss << std::put_time(&local_tm, "%m/%d/%Y, %I:%M:%S %p");
+        timestamp = oss.str();
+    }
 
     void viewSession() const {
         system("cls");
         std::cout << "Process Name: " << processName << "\n";
-        std::cout << "Instruction Line: " << currentLine << " / " << totalLines << "\n"; //hardcoded for now
+        std::cout << "Instruction Line: " << currentLine << " / " << totalLines << "\n";
         std::cout << "Timestamp: " << timestamp << "\n";
         std::cout << "Type 'exit' to return to main menu.\n";
     }
@@ -23,5 +36,8 @@ public:
         std::cout << "Process Name: " << processName << " | Created: " << timestamp << std::endl;
     }
 
-    std::string getprocessName() const { return processName; }
+    std::string getProcessName() const { return processName; }
+    std::string getTimestamp() const { return timestamp; }
+    int getCurrentLine() const { return currentLine; }
+    int getTotalLines() const { return totalLines; }
 };
